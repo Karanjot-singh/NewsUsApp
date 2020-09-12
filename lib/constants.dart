bf21af40b1a52f957e205d1df2edb0210c3eb8c2
karanjot-singh <karanjot19050@iiitd.ac.in>
2020-09-12 19:04:06 +0530

    Added file for constants

---
+++ lib/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+// Colors that we use in our app
+const kPrimaryColor = Color(0xffb1006a);
+const kAccentColor = Color(0xff00B147);
+const kTextColor = Color(0xFF3C4046);
+const kBackgroundColor = Color(0xFFF9F8FD);
+// For Customising icon Colours
+
+const double kDefaultPadding = 20.0;
