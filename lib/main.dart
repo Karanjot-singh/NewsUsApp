221c918542581fcff5504b65cbc45ab551c1b0c5
karanjot-singh <karanjot19050@iiitd.ac.in>
2020-09-12 13:29:12 +0530

    Fixed bug

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,29 @@
-import 'package:flutter/material.dart';
 
+import 'package:flutter/material.dart';
 import 'categoryHead.dart';
 
 void main() {
-  runApp(Concentric());
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'NewsUs',
+      // theme: ThemeData(
+      //   scaffoldBackgroundColor: kBackgroundColor,
+      //   primaryColor: kPrimaryColor,
+      //   accentColor: kAccentColor,
+      //   textTheme: Theme.of(context).textTheme.apply(bodyColor: kTextColor),
+      //   visualDensity: VisualDensity.adaptivePlatformDensity,
+      // ),
+      home: Concentric(),
+      // home:DetailsScreen(),
+      // home: VendorScreen(),
+
+    );
+  }
 }
